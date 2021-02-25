0a77b1c25783eed1fcb351e5907659f16f4e1ee1
nxoo <brianwangombe11@gmail.com>
2021-02-26 01:45:18 +0300

    env support for admin url

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / env('DATABASE_NAME'),
-    }
+    'default': env.db()
 }
 
 
--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import environ
 from django.contrib import admin
 from django.urls import path
 
+
+env = environ.Env()
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(env('ADMIN_URL'), admin.site.urls),
 ]
