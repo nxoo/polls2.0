c022b5de2484a7db90dcc85f43b12e13ce69ebfe
nxoo <brianwangombe11@gmail.com>
2021-02-26 02:11:05 +0300

    A view that does something

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, World! You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = '<br />'.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
