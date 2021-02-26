549dad6a86799fad8c54cb870b66261e8dec84b8
nxoo <brianwangombe11@gmail.com>
2021-02-26 15:37:40 +0300

    changed results view to generic

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -36,6 +36,6 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
