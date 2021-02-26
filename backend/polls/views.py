5386d9e36626a41a1be4101466cabf0efd008bc7
nxoo <brianwangombe11@gmail.com>
2021-02-26 13:48:19 +0300

    Detail view and template

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 
@@ -7,4 +6,9 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
