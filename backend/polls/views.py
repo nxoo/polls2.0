d6832ac2c4e200ae3c4da17d21a711775190f893
nxoo <brianwangombe11@gmail.com>
2021-02-26 13:26:29 +0300

    setup base html and index html & also changed index view to user render

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = '<br />'.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
