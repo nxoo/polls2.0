37de8f9b356f32e98f97a7da17a98449264d513d
nxoo <brianwangombe11@gmail.com>
2021-03-01 02:24:01 +0300

    added Detail view tests with future and past question

--- backend/polls/tests.py
+++ backend/polls/tests.py
@@ -50,6 +50,18 @@ class QuestionIndexViewTests(TestCase):
         )
 
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        question = create_question("future question", 5)
+        response = self.client.get(reverse('polls:detail', args=(question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        question = create_question("past question", -5)
+        response = self.client.get(reverse('polls:detail', args=(question.id,)))
+        self.assertEqual(response.status_code, 200)
+
+
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
