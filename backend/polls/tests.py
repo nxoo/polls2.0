3cfa5412813e919f9efb94685f5bb8936c863c2a
nxoo <brianwangombe11@gmail.com>
2021-02-28 23:02:00 +0300

    tests for indexview with no polls

--- backend/polls/tests.py
+++ backend/polls/tests.py
@@ -1,10 +1,28 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 from .models import Question
 
 
-class QuestionModelTest(TestCase):
+def create_question(question_text, days):
+    """
+    create a question with the given `question_text` and publish the given number of `days`
+    offset to now, negative days for past, positive days for future
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, days=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertIs(response.status_code, 200)
+        self.assertContains(response, "There are no polls.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+
+class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns false for questions whose pub_date is in the future
