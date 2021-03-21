43e55418ec468df6e5ebe8cba80a9001c65a92f0
nxoo <brianwangombe11@gmail.com>
2021-03-22 00:04:37 +0300

    changed polls template app to use  instead of , added  to QuestionViewSet, added  view to ChoiceViewSet

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -3,8 +3,30 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponseRedirect
 from django.views import generic
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Question, Choice
-from .serializers import QuestionSerializer
+from .serializers import QuestionSerializer, ChoiceSerializer
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ChoiceViewSet(viewsets.ModelViewSet):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+    @action(detail=True)
+    def vote(self, request, *args, **kwargs):
+        choice = self.get_object()
+        choice.votes += 1
+        choice.save()
+        return Response(ChoiceSerializer(choice).data)
 
 
 class IndexView(generic.ListView):
@@ -15,7 +37,7 @@ class IndexView(generic.ListView):
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).exclude(
-            choice__isnull=True
+            choices__isnull=True
         ).order_by('-pub_date')
 
 
@@ -27,7 +49,7 @@ class DetailView(generic.DetailView):
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).exclude(
-            choice__isnull=True
+            choices__isnull=True
         )
 
 
@@ -39,15 +61,15 @@ class ResultsView(generic.DetailView):
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).exclude(
-            choice__isnull=True
+            choices__isnull=True
         )
 
 
 def vote(request, question_id):
-    question_model = Question.objects.filter(pub_date__lte=timezone.now()).exclude(choice__isnull=True)
+    question_model = Question.objects.filter(pub_date__lte=timezone.now()).exclude(choices__isnull=True)
     question = get_object_or_404(question_model, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choices.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'error_message': "You didn't select a choice",
@@ -62,7 +84,3 @@ def vote(request, question_id):
         """
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-
-class QuestionViewSet(viewsets.ModelViewSet):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
