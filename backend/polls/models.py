af43debefedf7ba81dfa96acc79be7128cacd2f8
nxoo <brianwangombe11@gmail.com>
2021-03-21 23:54:59 +0300

    added related name  to Choice model  foreign key

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', default=timezone.now)
 
     def __str__(self):
         return self.question_text
@@ -19,9 +19,10 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE, blank=True)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
