d8c35b61b51cfbc1114bb05b3d5606157c86ca66
nxoo <brianwangombe11@gmail.com>
2021-05-14 19:34:59 +0300

    bootstrap working fine

---
+++ frontend/components/layout.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+
+export default function Layout({children}) {
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <div className="container">
+                {children}
+            </div>
+        </>
+    )
+}
