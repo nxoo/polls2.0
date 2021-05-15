ff551afb4c9f2297d9c59ed894245cad43bd093e
nxoo <brianwangombe11@gmail.com>
2021-05-15 17:51:00 +0300

    a custom 404 page

---
+++ frontend/pages/404.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Layout from "../components/layout";
+
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <div className="custom404">
+                <h4>404 - Page Not Found</h4>
+                <Link href="/">
+                    <a>← Back to home</a>
+                </Link>
+            </div>
+            <style jsx>{`
+                .custom404 {
+                    text-align: center;
+                    margin-top: 20%;
+                }
+            `}</style>
+        </Layout>
+    )
+}
