841ccc06b7322ade0c408dba9c2a044173799745
nxoo <brianwangombe11@gmail.com>
2021-05-16 05:44:45 +0300

    removed align-center from 404 page

--- frontend/pages/404.js
+++ frontend/pages/404.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout";
 export default function Custom404() {
     return (
         <Layout>
-            <div className="custom404">
+            <div className="custom404 p-2">
                 <h4>404 - Page Not Found</h4>
                 <Link href="/">
                     <a>← Back home</a>
@@ -13,8 +13,7 @@ export default function Custom404() {
             </div>
             <style jsx>{`
                 .custom404 {
-                    text-align: center;
-                    margin-top: 20%;
+                    margin-top: 10%;
                 }
             `}</style>
         </Layout>
