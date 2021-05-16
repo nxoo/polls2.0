293b77ce0b727ae0b137b5693db0423aaa0459de
nxoo <brianwangombe11@gmail.com>
2021-05-16 05:53:54 +0300

    minor changes

--- frontend/lib/polls.js
+++ frontend/lib/polls.js
@@ -1,12 +1,15 @@
 import axios from "axios";
+import {error} from "next/dist/build/output/log";
 
 export async function getPolls() {
     const res = await axios.get(`${process.env.HOST}/api/questions/`)
+        .catch(error => console.log(error))
     return res.data
 }
 
 export async function getAllPollsIds() {
     const polls = await axios.get(`${process.env.HOST}/api/questions/`)
+        .catch(error => console.log(error))
     return polls.data.map(poll => {
         return {
             params: {
@@ -19,8 +22,5 @@ export async function getAllPollsIds() {
 export async function getPollsData(id) {
     const res = await axios.get(`${process.env.HOST}/api/questions/${id}/`)
         .catch(error => console.log(error))
-    return {
-        id,
-        ...res.data
-    }
+    return res.data
 }
