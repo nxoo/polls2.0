62bd86e9729939e5051162f1339502bc0c2e2e9a
nxoo <brianwangombe11@gmail.com>
2021-05-18 12:39:53 +0300

    poll detail working

--- frontend/lib/polls.js
+++ frontend/lib/polls.js
@@ -2,25 +2,37 @@ import axios from "axios";
 import {error} from "next/dist/build/output/log";
 
 export async function getPolls() {
-    const res = await axios.get(`${process.env.HOST}/api/questions/`)
-        .catch(error => console.log(error))
-    return res.data
+    try {
+        const res = await axios.get(`${process.env.HOST}/api/questions/`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+        return null
+    }
 }
 
 export async function getAllPollsIds() {
-    const polls = await axios.get(`${process.env.HOST}/api/questions/`)
-        .catch(error => console.log(error))
-    return polls.data.map(poll => {
-        return {
-            params: {
-                id: poll.id.toString()
+    try {
+        const polls = await axios.get(`${process.env.HOST}/api/questions/`)
+        return polls.data.map(poll => {
+            return {
+                params: {
+                    id: poll.id.toString()
+                }
             }
-        }
-    })
+        })
+    } catch (error) {
+        console.log(error)
+        return []
+    }
 }
 
 export async function getPollsData(id) {
-    const res = await axios.get(`${process.env.HOST}/api/questions/${id}/`)
-        .catch(error => console.log(error))
-    return res.data
+    try {
+        const res = await axios.get(`${process.env.HOST}/api/questions/${id}/`)
+        return res.data
+    } catch (error) {
+        console.log(error)
+        return null
+    }
 }
--- frontend/pages/polls/[id].js
+++ frontend/pages/polls/[id].js
@@ -1,30 +1,50 @@
+import Link from "next/link";
 import Layout from "../../components/layout";
 import {getAllPollsIds, getPollsData} from "../../lib/polls";
 import Date from "../../components/date";
-import Link from "next/link";
 
 
 const maybePluralize = (count, noun, suffix = 's') =>
     `${count} ${noun}${count !== 1 ? suffix : ''}`;
 
 export default function Poll({pollData}) {
+    if (pollData === null) {
+        return (
+            <Layout>
+                <div>Error fetching data!</div>
+            </Layout>
+        )
+
+    }
     return (
         <Layout>
-            <div className="p-2">
-                <h4>{pollData.question_text}</h4>
-                <span className="date">By {pollData.owner} on <Date dateString={pollData.pub_date}/></span>
-                <ol type='a'>
-                    {pollData.choices.map(({id, choice_text, votes}) => (
-                        <li key={id}>{choice_text} -- {maybePluralize(votes, 'vote')}</li>
+            <div className="p-lg-2">
+                <h2>{pollData.question_text}</h2>
+                <p className="date">
+                    By <Link href={`/u/${pollData.owner}`}><a>{pollData.owner}</a></Link> on{' '}
+                    <Date dateString={pollData.pub_date}/>
+                </p>
+                <form method='post'>
+                    {pollData.choices.map(({id, choice_text}) => (
+                        <div className="form-check" key={id}>
+                            <input type="radio" name="vote" id={id} className="form-check-input" required />
+                            <label htmlFor={id} className="form-check-label">{choice_text}</label>
+                        </div>
                     ))}
-                </ol>
+                    <br/>
+                    <input type="submit" value="vote" className="btn btn-success"/>
+                </form>
+                <br />
                 <Link href="/">
                     <a>← Back home</a>
+                </Link> {' | '}
+                <Link href={`/${pollData.id}/results`}>
+                    <a>Results</a>
                 </Link>
             </div>
             <style jsx>{`
               .date {
-                font-size: x-small;
+                font-size: small;
               }
             `}</style>
         </Layout>
